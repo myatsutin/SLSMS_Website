ed8ecd6ded85fe027ced23af5a3eeb208700ddcc
MYATSUTIN <myatsutin99@gmail.com>
2018-05-27 16:19:06 +0800

    NodeJob
    First draft of the job system

--- SLSMS_Website/models/Client.js
+++ SLSMS_Website/models/Client.js
@@ -1,31 +1,9 @@
-// // var mongoose = require('mongoose'),
-// //     Schema = mongoose.Schema,
-// //     soft_delete = require('mongoose-softdelete');
-//
-// var mongoose = require('mongoose');
-//
-// var ClientScheme = new mongoose.Schema({
-//   client_id: String,
-//   company: String,
-//   address: String,
-//   country: String,
-//   city: String,
-//   officeno: String,
-//   website: String,
-//   status: String,
-//   photo:String,
-//   updated_at: { type: Date, default: Date.now }
-//
-// });
-//
-// // ClientScheme.plugin(soft_delete);
-// module.exports = mongoose.model('Client', ClientScheme, 'clients');
 var mongoose = require('mongoose');
 
 
 var ClientScheme = new mongoose.Schema({
   client_id: String,
-  company: String,
+  companyname: String,
   address: String,
   country: String,
   city: String,
